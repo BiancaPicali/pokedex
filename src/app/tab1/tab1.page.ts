d3f35d01896f19f1b99a28f667091aba8e55fa05
Etec 244 <e244ti@outlook.com>
2021-08-13 15:00:02 -0300

    13/08/21
    toast and alert. adicionando conteúdos programáveis

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,37 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController, public toastController: ToastController) {}
+  async presentAlertFavorito() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'alerta',
+      message: 'deseja realmente favoritar o pokemon?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Favoritar',
+          handler: () => {
+            this.presentToast();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Pokemon adicionado aos favoritos',
+      duration: 1000,
+      color:'danger'
+    });
+    toast.present();
+  }
 
 }
